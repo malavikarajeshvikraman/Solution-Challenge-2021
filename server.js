55f31ca6a8eeb406808af1cf884c2cc58dd0f594
Sanjsarma <sangeethasanjana@gmail.com>
2021-03-29 18:04:27 +0530

    Restart, new scholarship

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(fileUpload());
 const conn=mysql.createConnection({
     host:'localhost',
     user: 'root',
-    password: '12345',
+    password: 'sanjana123',
     database: 'challenge'
 })
 app.set('view engine','ejs');
