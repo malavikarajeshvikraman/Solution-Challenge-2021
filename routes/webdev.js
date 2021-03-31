699c57818ec76ce5a31c7fb21d989ff2b06ae492
malavikarajeshvikraman <malu.rajesh2000@gmail.com>
2021-04-01 00:17:54 +0530

    trial

--- routes/webdev.js
+++ routes/webdev.js
@@ -1,9 +1,13 @@
 var express= require('express');
 var router=express.Router();
+var db=require('../db/data');
 
 
 router.get("/webdev",function(req, res){
-    res.render("webdev.ejs");
+    var sql5 = 'select * from  persons  where Aoe = "Web Development";';
+    db.query(sql5, function (err, data1) {
+        res.render('webdev',{data1:data1});
+            });
 });
 
 module.exports = router;
